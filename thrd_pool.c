c2f940333c0c569a3316e0a70d72a1e0de83fa0f
hakase <cb634780751@qq.com>
2021-04-22 16:07:11 +0800

    Update thrd_pool.c

--- thrd_pool.c
+++ thrd_pool.c
@@ -1,114 +1,86 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-struct thrd_task_t
-{
-	void * (*fn)(void*);
+typedef struct {
+	void * (*func)(void*);
 	void *args;
-};
+} thrd_task_t;
 
-struct thrd_pool_t
-{
-	size_t stat;
-	size_t max_thrd_num;
+typedef struct {
+	int stat;
+	size_t size;
 	pthread_t *thrds;
-	struct thrd_task_t *task_hdr;
-	pthread_cond_t que_rdy;
-	pthread_mutex_t que_mtx;
-};
+	pthread_mutex_t task_mtx;
+	pthread_cond_t task_cond;
+} thrd_pool_t;
 
-static void *task_routine(void *args)
+static void *thrd_task_routine(void *args)
 {
-	struct thrd_pool_t *pool = args;
-	struct thrd_task_t *task = NULL;
+	thrd_pool_t *pool;
+	thrd_task_t *task;
 	
-	while (1) {
-		pthread_mutex_lock(&pool->que_mtx);
-		while (!pool->task_hdr) {
-			pthread_cond_wait(&pool->que_rdy, &pool->que_mtx);
-		}
-		
-		if (!pool->stat) {
-			pthread_mutex_unlock(&pool->que_mtx);
+	pool = args;
+	for (;;) {
+		pthread_mutex_lock(&pool->task_mtx);
+		if (!q.empty()) {
+			task = q.front();
+			q.pop();
+			pthread_mutex_unlock(&pool->task_mtx);
+			task->func(task->args);	
+		} else if (pool->stat == 0) {
+			pthread_mutex_unlock(&pool->task_mtx);
 			break;
+		} else {
+			pthread_cond_wait(&pool->task_cond, &pool->task_mtx);
+			pthread_mutex_unlock(&pool->task_mtx);
 		}
-		
-		/* pop from data struct */
-		
-		pthread_mutex_unlock(&pool->que_mtx);
-		
-		task->fn(task->args);
-		free(task);
 	}
+	pthread_exit(NULL);
 	return NULL;
 }
 
-int thrd_pool_init(thrd_pool_t **pool, size_t max_thrd_num, void* (*fn)(void*))
+thrd_pool_t* thrd_pool_init(size_t size)
 {
 	int ret, idx;
+	thrd_pool_t *pool;
 	
-	*pool = malloc(sizeof(struct thrd_pool_t));
-	if (NULL == *pool) return -1;
+	pool = malloc(sizeof(thrd_pool_t));
+	pool->stat = 1;
+	pool->size = size;
+	pool->thrds = malloc(sizeof(pthread_t) * pool->size);
+	ret = pthread_mutex_init(&pool->task_mtx, NULL);
+	if (ret == -1) return NULL; 
+	ret = pthread_cond_init(&pool->task_cond, NULL);
+	if (ret == -1) return NULL;
 	
-	(*pool)->max_thrd_num = max_thrd_num;
-	(*pool)->thrds = malloc(sizeof(pthread_t) * max_thrd_num);
-	if (NULL == (*pool)->thrds) return -2;
-	
-	ret = pthread_mutex_init(&(*pool)->que_mtx, NULL);
-	if (-1 == ret) return -3;
-	
-	ret = pthread_cond_init(&(*pool)->que_rdy, NULL);
-	if (-1 == ret) return -4;
-	
-	for (idx = 0; idx < max_thrd_num; ++idx) {
-		ret = pthread_create(&(*pool)->thrds[idx], NULL, fn, *pool);
-		if (-1 == ret) return -5;
+	for (idx = 0; idx < pool->size; ++idx) {
+		ret = pthread_create(&pool->thrds[idx], NULL, thrd_task_routine, pool);
+		if (-1 == ret) return NULL;
 	}
-	return 0;
+	return pool;
 }
 
-int thrd_pool_destroy(thrd_pool_t *pool)
+void thrd_pool_destroy(thrd_pool_t *pool)
 {
 	int idx;
 	
-	if (NULL == pool) return -1;
-	if (!pool->stat) return -2;
-	
+	pthread_mutex_lock(&pool->task_mtx);
 	pool->stat = 0;
-	pthread_mutex_lock(&pool->que_mtx);
-	pthread_cond_broadcast(&pool->que_rdy);
-	pthread_mutex_unlock(&pool->que_mtx);
-	
-	for (idx = 0; idx < pool->max_thrd_num; ++idx) {
+	pthread_mutex_unlock(&pool->task_mtx);
+	pthread_cond_broadcast(&pool->task_cond);
+	for (idx = 0; idx < pool->size; ++idx) {
 		pthread_join(pool->thrds[idx], NULL);
 	}
+	pthread_mutex_destroy(&pool->task_mtx);
+	pthread_cond_destroy(&pool->task_cond);
 	free(pool->thrds);
-	
-	/* free data struct */
-	
-	pthread_mutex_destroy(&pool->que_mtx);
-	pthread_cond_destroy(&pool->que_rdy);
 	free(pool);
-	return 0;
 }
 
-int thrd_pool_add_task(thrd_pool_t *pool, void *(*fn)(void*), void *args)
+void thrd_pool_add_task(thrd_pool_t *pool, thrd_task_t *task)
 {
-	thrd_task_t *task;
-	
-	if (NULL == fn) return -1;
-	
-	task = malloc(sizeof(thrd_task_t));
-	if (NULL == task) return -2;
-	
-	task->fn = fn;
-	task->args = args;
-	
-	pthread_mutex_lock(&pool->que_mtx);
-	
-	/* push to data struct */
-	
-	pthread_cond_signal(&pool->que_rdy);
-	pthread_mutex_unlock(&pool->que_mtx);
-	return 0;
+	pthread_mutex_lock(&pool->task_mtx);
+	q.push(task);
+	pthread_cond_signal(&pool->task_cond);
+	pthread_mutex_unlock(&pool->task_mtx);
 }
